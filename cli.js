8e7d0e539d7e0367da319d1af4ed637d493f17f8
r0b0t <antoniomainakn@gmail.com>
2018-11-29 14:11:14 +0300

    Added chalk to console logs.

--- cli.js
+++ cli.js
@@ -1,31 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk');
 
 function createDirectories() {
     /**
      * Create ITCSS directories.
      */
     fs.mkdir('settings', function () {
-        console.log("Created the settings folder.");
+        console.log(chalk.bgGreen("Created the settings folder."));
     });
     fs.mkdir('tools', function () {
-        console.log("Created the tools folder.");
+        console.log(chalk.bgGreen("Created the tools folder."));
     });
     fs.mkdir('generic', function () {
-        console.log("Created the generic folder.");
+        console.log(chalk.bgGreen("Created the generic folder."));
     });
     fs.mkdir('elements', function () {
-        console.log("Created the elements folder.");
+        console.log(chalk.bgGreen("Created the elements folder."));
     });
     fs.mkdir('objects', function () {
-        console.log("Created the objects folder.");
+        console.log(chalk.bgGreen("Created the objects folder."));
     });
     fs.mkdir('components', function () {
-        console.log("Created the components folder.");
+        console.log(chalk.bgGreen("Created the components folder."));
     });
     fs.mkdir('utilities', function () {
-        console.log("Created the utilities folder.");
+        console.log(chalk.bgGreen("Created the utilities folder."));
     });
 }
 
@@ -35,37 +36,36 @@ function createFiles() {
      */
     fs.open('settings/_settings.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created settings/_settings.scss file');
+        console.log(chalk.green('Created settings/_settings.scss file'));
     });
     fs.open('tools/_tools.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created tools/_tools.scss file');
+        console.log(chalk.green('Created tools/_tools.scss file'));
     });
     fs.open('generic/_generic.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created generic/_generic.scss file');
+        console.log(chalk.green('Created generic/_generic.scss file'));
     });
     fs.open('elements/_elements.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created elements/_elements.scss file');
+        console.log(chalk.green('Created elements/_elements.scss file'));
     });
     fs.open('objects/_objects.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created objects/_objects.scss file');
+        console.log(chalk.green('Created objects/_objects.scss file'));
     });
     fs.open('components/_components.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created components/_components.scss file');
+        console.log(chalk.green('Created components/_components.scss file'));
     });
     fs.open('utilities/_utilities.scss', 'w', function (err, file) {
         if (err) throw err;
-        console.log('Created utilities/_utilities.scss file');
+        console.log(chalk.green('Created utilities/_utilities.scss file'));
     });
 }
 
-createDirectories();
-createFiles();
-
 exports.createDirectories = createDirectories();
 exports.createFiles = createFiles();
+
+console.log(chalk.bold("Thank You for using @r0b0t/itcss.\n"));
 
